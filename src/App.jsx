447c7ed36e763701377a26db363c3adae7437e3f
ahmd01 <ahmed.adl007@gmail.com>
2019-08-31 14:30:12 -0400

    updated the ladningPage

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Footer from "./components/Footer";
 import MyCarousel from "./components/carousel"
 import Blank from "./components/Blank";
 import Grid from "./components/Grid";
-import PrayerTimes from "./components/PrayerTimes";
 
 //TODO Web Template Studio: Add routes for your new pages here.
 class App extends Component {
